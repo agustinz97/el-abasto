3e3725e52a60a2a868e499b17cfb9c67c527933d
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-05 17:18:31 -0300

    dashboard

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -41,27 +41,7 @@
                     <a href="{{route('proveedores.index')}}">Proveedores</a>
                 </div>
             </div>
-        </div>
-        <div class="col-md-4 mt-3">
-            <div class="card">
-                <div class="card-body text-center p-3">
-                    <div class="icon mb-3">
-                        <i class="fas fa-user-friends"></i>
-                    </div>
-                    <a href="#">Clientes</a>
-                </div>
-            </div>
-        </div>
-        <div class="col-md-4 mt-3">
-            <div class="card">
-                <div class="card-body text-center p-3">
-                    <div class="icon mb-3">
-                        <i class="fas fa-box-open"></i>
-                    </div>
-                    <a href="#">Pedidos</a>
-                </div>
-            </div>
-        </div>
+        </div>        
     </div>
 </div>
 @endsection
