4e165e9a3d9b71adff87d8272a28e7c2e6a0a819
Agustin Zamar <agustinzamar33@gmail.com>
2020-05-05 21:26:10 -0300

    marcas and proveedores name ucfirst

--- app/Marca.php
+++ app/Marca.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Marca extends Model
 {
-    protected $table = 'marcas';
+	protected $table = 'marcas';
+	
+	public function getNameAttribute($value){
+		return ucfirst($value);
+	}
 
     public function productos(){
         return $this->hasMany('App\Producto');
--- app/Proveedor.php
+++ app/Proveedor.php
@@ -6,7 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Proveedor extends Model
 {
-    protected $table = 'proveedores';
+	protected $table = 'proveedores';
+	
+	public function getNameAttribute($value){
+		return ucfirst($value);
+	}
 
     public function productos(){
         return $this->belongsToMany('App\Producto', 'productos_proveedores' )->withPivot('price');
